4dc130b1f652863e8dec19c775b4293d1239d47b
Trump2022 <qsw2012@outlook.com>
2023-05-29 19:03:20 +0800

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -70,4 +70,7 @@ Route::resource('replies', 'RepliesController', ['only' => ['store','destroy']])
 
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
 
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
+
+
 
